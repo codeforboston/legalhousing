2165070b5dc64cc23ea5feb10d54cfefef21d4a9
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-02-07 20:44:57 -0500

    Resolve #21 Return Legal Status
    This commit adds a method that uses regular expressions to determine whether we believe a listing is illegal.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,16 @@
 class Listing < ApplicationRecord
+  def illegal?
+    case self.description
+      when /no\s*section\s*(8|eight)/i
+        true
+      when /professionals\s*only/i
+        true
+      when /no\s*students/i
+        true
+      when /no\s*couples/i
+        true
+      else
+        return false
+    end
+  end
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'no section eight' do
+    it 'marks it as illegal' do
+      listing = build(:listing, description: 'No section eight')
+      expect(listing.illegal?).to be_truthy
+    end
+  end
+  context 'professionals only' do
+    it 'marks it as illegal' do
+      listing = build(:listing, description: 'professionals only')
+      expect(listing.illegal?).to be_truthy
+    end
+  end
+  context 'no students' do
+    it 'marks it as illegal' do
+      listing = build(:listing, description: 'no students')
+      expect(listing.illegal?).to be_truthy
+    end
+  end
+  context 'no couples' do
+    it 'marks it as illegal' do
+      listing = build(:listing, description: 'no couples')
+      expect(listing.illegal?).to be_truthy
+    end
+  end
 end

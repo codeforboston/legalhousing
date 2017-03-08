037a8f5e32061f4b46588cd2d521b1016b25e00f
Tom Barto <tom.barto@outlook.com>
2017-03-07 23:34:23 -0500

    added python script to upload records to local database

--- assets/upload_to_local_db.py
+++ assets/upload_to_local_db.py
@@ -1,10 +1,13 @@
+#command line usage: python script_filename int
+#sys.argv[1] is the number of listings to upload to the local database
+
 import requests, sys, json, time
 from datetime import datetime
 
 class Listing:
     def __init__(self, address, listed_at, latitude, longitude, description, heading):
         self.address = address
-        self.listed_at = datetime.fromtimestamp(float(listed_at)).strftime('%Y-%m-%d %H:%M:%S %Z')
+        self.listed_at = listed_at
         self.latitude = latitude
         self.longitude = longitude
         self.description = description
@@ -19,21 +22,24 @@ class Listing:
             'longitude': self.longitude,
             'description': self.description,
             'heading': self.heading,
-            'discriminatory': self.discriminatory
+            'discriminatory': self.discriminatory,
+            'listed_at': self.listed_at
             }
 
 if __name__ == '__main__':
-    json_name = sys.argv[1]
 
-    with open(json_name) as f:
-        data = json.load(f)
+    j = int(sys.argv[1])
 
-    url = 'https://legalhousing.herokuapp.com/listings'
+    get_data = requests.get('https://legalhousing.herokuapp.com/listings.json')
+    data = json.loads(get_data.content)
 
-    for i in range(1000):
+    #local database url to post to - change port if necessary
+    url = 'http://localhost:3000/listings.json'
+
+    for i in range(j):
         listing = data[i]
-        listing_model = Listing(listing['address'], listing['timestamp'], listing['lat'], listing['long'],
-            listing['description'], listing['heading'])
+        listing_model = Listing(listing['address'], listing['listed_at'], listing['latitude'], listing['longitude'],
+        listing['description'], listing['heading'])
 
         headers = {'content-Type': 'application/json'}
         r = requests.post(url,

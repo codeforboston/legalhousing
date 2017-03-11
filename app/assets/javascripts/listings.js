62982862d16fb70898a8f0c1f821d3c965bc42dd
Abraham Rubin <abrahamgrubin@gmail.com>
2017-03-11 18:25:34 -0500

    Started to create sorting method for the table; created more random seeding so I can work with the sort; added class names to the table

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+    var headingLabel = document.getElementsByClassName('heading-label')[0],
+        dateTimeLabel = document.getElementsByClassName('date-time-label')[0],
+        addressLabel = document.getElementsByClassName('address-label')[0],
+        latitudeLabel = document.getElementsByClassName('latitude-label')[0],
+        longitudeLabel = document.getElementsByClassName('longitude-label')[0],
+        descriptionLabel = document.getElementsByClassName('description-label')[0],
+        descriminatoryLabel = document.getElementsByClassName('descriminatory-label')[0],
+        latitudeItem = document.getElementsByClassName('latitude-item'),
+        descriptionItem = document.getElementsByClassName('description-item');
+
+    function sortItem(it){
+        var newArr = [];
+        for(i = 0; i < it.length; i++){
+            newArr.push(it[i].innerHTML);
+        }
+        newArr.sort();
+    };
+    latitudeLabel.addEventListener('click', sortItem(descriptionItem));
+});

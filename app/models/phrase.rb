5a225da462af9640fd9cdcd4a247874df0036aa6
smithwebtek <brad@smithwebtek.com>
2017-04-04 00:24:57 -0400

    remove phrase-test

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,10 +1,6 @@
 require 'csv'
 class Phrase < ApplicationRecord
 
-  def self.phrase_test
-    @phrase = Phrase.find_or_create_by(content: "test phrase")
-  end
-
   def self.import_words
     wordsfile = File.join Rails.root, "./assets/Words.txt"
     CSV.read(wordsfile)[0].each do |word|

83d1074d4e3ecb2c1e8192a291c5dcb9741052f3
kfili <kosta.filiotis@gmail.com>
2017-04-04 22:33:52 -0400

    Add create method to scrapers controller
    - add a button on listings/index.html to run the scraper
    - scraper now runs adds data that appears the view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get '/discriminatory', to: 'listings#discriminatory'
   root to: "listings#home"
   get '/visualization', to: 'listings#visualization'
-  get '/scrape' to: 'scrapers#show'
   # get 'listings/show'
   # get 'listings/edit'
   # get 'listings/update'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125023531) do
+ActiveRecord::Schema.define(version: 20170329005001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20170125023531) do
     t.boolean  "discriminatory"
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
+  end
+
+  create_table "scrapers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

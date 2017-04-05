369caad51afd4e61ab53d04c12b951f6b559580e
jd2rogers2 <jd2rogers2@gmail.com>
2017-04-04 20:49:41 -0400

    added google maps api to visualizations page and accompanying scss file and scripts&divs on visualization.erb.html

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -5,7 +5,7 @@ var loadData = function(){
     dataType: 'json',
     success: function(data){
       var listings = data.filter(function(point){
-        return point.latitude;
+        return point.latitude && point.discriminatory;
       });
       drawPieChart(data);
       initMap(listings);
@@ -18,26 +18,37 @@ var loadData = function(){
 
 function initMap(listings) {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 3,
-    center: {lat: 42.3601, lng: 71.0589}
+    zoom: 11,
+    center: {lat: 42.3601, lng: -71.0589}
   });
-
-  // Create an array of alphabetical characters used to label the markers.
   var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-  // Add some markers to the map.
-  // Note: The code uses the JavaScript Array.prototype.map() method to
-  // create an array of markers based on a given "locations" array.
-  // The map() method here has nothing to do with the Google Maps API.
-  var markers = listings.map(function(location, i) {
-    var temp = {lat: parseFloat(location.latitude), lng: parseFloat(location.longitude)}
-    return new google.maps.Marker({
-      position: temp,
-      label: labels[i % labels.length]
+  if (listings) {
+    var markers = listings.map(function(location, i) {
+      var temp = {lat: parseFloat(location.latitude), lng: parseFloat(location.longitude)}
+      return new google.maps.Marker({
+        position: temp,
+        label: labels[i % labels.length]
+      });
     });
-  });
+    var geoJson = [];
+    for (var listing in listings) {
+      var temp = {
+        type: 'Feature',
+        geometry: {type: 'Point', coordinates: [parseInt(listing.latitude, 10), parseInt(listing.longitude, 10)]},
+        properties: {name: 'address: ' + listing.address}
+      }
+      geoJson.push(temp);
+    }
+    map.data.addGeoJson({type: 'FeatureCollection', features: geoJson});
 
-  // Add a marker clusterer to manage the markers.
+    var coordsDiv = document.getElementById('coords');
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(coordsDiv);
+    map.addListener('mousemove', function(event) {
+      coordsDiv.textContent =
+          'lat: ' + Math.round(event.latLng.lat()) + ', ' +
+          'lng: ' + Math.round(event.latLng.lng());
+    });
+  }
   var markerCluster = new MarkerClusterer(map, markers,
       {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }

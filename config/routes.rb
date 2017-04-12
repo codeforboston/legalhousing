68b8703a94db89f18fe5adaed56f0704b1fc383b
jd2rogers2 <jd2rogers2@gmail.com>
2017-04-11 20:20:22 -0400

    reviewed code. removed empty files and empty spaces, approved new methods etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   root to: "listings#home"
-
-  
   resources :listings, :phrases
   get '/discriminatory', to: 'listings#discriminatory'
   get '/visualization', to: 'listings#visualization'
-  # get 'listings/show'
-  # get 'listings/edit'
-  # get 'listings/update'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/phrases_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PhrasesController, type: :controller do
-
-end

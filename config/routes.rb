2de398c70dc8f0ccc6551438019f7003b50cca90
kfili <kosta.filiotis@gmail.com>
2017-05-23 21:03:48 -0400

    Make route for 'get stats'

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :listings, :phrases
   get '/discriminatory', to: 'listings#discriminatory'
   get '/visualization', to: 'listings#visualization'
+  get '/tools/stats', to: 'stats#get_stats'
 end

7b0414813374a463e16fde8d9922312e61b000c9
sujkid <suja.sreekumar@yahoo.com>
2017-06-13 20:33:23 -0400

    Adds a new view for stats
    Implements the get_stats method in the Stats
    Controller. It checks if the token exists in the hash
    and returns it if it does, else computes the value, adds it to hash
    and returns it.
    
    Makes an ajax call(POST) to tools/stats.json that invokes the get_stats
    method in the controller.
    
    Renames a migration file.

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var getStats = function(){
+  $.ajax({
+    type: 'POST',
+    url: '/tools/stats.json',
+    data: {token: "3m"},
+    async: false,
+    dataType: 'json',
+    success: function(data){
+      console.log(data);
+    },
+    failure: function(result){
+      alert('ERROR');
+    }
+  });
+};

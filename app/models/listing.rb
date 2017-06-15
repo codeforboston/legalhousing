c67e78e70cadb917857fb3a0e3a7d06008ce1d40
smithwebtek <brad@smithwebtek.com>
2017-06-15 11:29:40 -0400

    update 'illegal?' method to implement PhraseListing join table entry for each instance of phrase appearing in a listing description; set_discriminatory provides method to run all Listings through 'illegal?' method

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,8 @@ class Listing < ApplicationRecord
   def illegal?
     Phrase.all.each do |phrase|
       if self.description.match(/#{phrase.content}/i)
+				PhraseListing.create(listing_id: self.id, phrase_id: phrase.id)				
+				self.save
         return true
       end
     end
@@ -21,6 +23,12 @@ class Listing < ApplicationRecord
     end
   end
 
+	def self.set_discriminatory
+		Listing.all.each do |listing|
+			listing.illegal?
+		end
+	end
+	
   def self.discriminatory
     Listing.all.select {|listing| listing.discriminatory == true}
   end

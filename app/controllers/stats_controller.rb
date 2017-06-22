04c1f104d8d455497404789e0543809a166ac3ce
sujkid <suja.sreekumar@yahoo.com>
2017-06-21 21:23:00 -0400

    Calculate listings
    Calculate listings by using the Listing.date_range function.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,23 +1,29 @@
+# require 'Date'
 # stats controller
 class StatsController < ApplicationController
-
   # @@cashedStats = Hash.new(0)
 
-	def compute_stats
-		@num_listings = Listing.num_listings
-		@num_discriminatory = Listing.num_discriminatory
-		@stats = {
-			num_listings: @num_listings,
-			num_discriminatory: @num_discriminatory
-		}
-		@stats
-	end
+  def compute_stats
+    @num_listings = Listing.num_listings
+    @num_discriminatory = Listing.num_discriminatory
+    @stats = {
+      num_listings: @num_listings,
+      num_discriminatory: @num_discriminatory
+    }
+    @stats
+  end
 
   def index
   end
 
   def get_stats
     token = params[:token]
+
+    end_date = Date::today
+    start_date = end_date << 3
+    listings = Listing.date_range( start_date.to_s, end_date.to_s )
+
+    # binding.pry
 
     stats = ''
     # check if token exists in hash

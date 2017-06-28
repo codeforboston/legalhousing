2f93092998145f797115640d7cd245b130eadcaf
sujkid <suja.sreekumar@yahoo.com>
2017-06-27 20:46:54 -0400

    Calculate end_date and start_date and create listings
    Calculate end_date and start_date and call the date_range functions
    to create the listings in the given date range

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,7 @@
 # require 'Date'
 # stats controller
 class StatsController < ApplicationController
-  # @@cashedStats = Hash.new(0)
+  # @@cashedStats = {}
 
   def compute_stats
     @num_listings = Listing.num_listings
@@ -18,14 +18,12 @@ class StatsController < ApplicationController
 
   def get_stats
     token = params[:token]
-
+    # token.slice! 'm'
     end_date = Date::today
     start_date = end_date << 3
     listings = Listing.date_range( start_date.to_s, end_date.to_s )
-
-    # binding.pry
-
     stats = ''
+    
     # check if token exists in hash
     # if @@cashedStats.key?(token)
       # if yes, return the value

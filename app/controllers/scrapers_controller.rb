eea4b724f1fca990d7d4bc3c7fd9476f80ea5ce5
Brian Sanders <bdougsand+cfb@gmail.com>
2017-08-08 20:53:23 -0400

    Update scraper counts instead of creating
    When a scraper runs, add the new listing count to the scraper's existing
    count, instead of creating a new record for each scrape.

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -16,10 +16,15 @@ class ScrapersController < ApplicationController
     end
 
     scraper_info.each do |site_name, number_of_listings|
-      Scrape.create(
-        site: site_name,
-        number_of_listings: number_of_listings
-      )
+      scrape = Scrape.find_by(site: site_name)
+      if scrape
+        scrape.update(number_of_listings: scrape.number_of_listings + number_of_listings)
+      else
+        Scrape.create(
+          site: site_name,
+          number_of_listings: number_of_listings
+        )
+      end
     end
 
     redirect_to listings_path

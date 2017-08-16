b6cef12a607ec82666880bec097588f101735129
Steve Hackbarth <stephenhackbarth@gmail.com>
2017-08-15 20:50:48 -0400

    bring back pie chart

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,27 +1,34 @@
-// var loadData = function(){
-//   $.ajax({
-//     type: 'GET',
-//     url: '/visualization.json',
-//     async: false,
-//     dataType: 'json',
-//     success: function(data){
-//       drawPieChart(data);
-//       console.log('success');
-//     },
-//     failure: function(result){
-//       alert('ERROR');
-//     }
-//   })
-// }
-// function drawPieChart(data){
-//   d3plus.viz()
-//     .container("#viz")
-//     .data(data)
-//     .type("pie")
-//     .id("discriminatory")
-//     .size("id")
-//     .draw()
-// }
-// $(document).ready(function(){
-//   loadData();
-// })
+var loadData = function(){
+  $.ajax({
+    type: 'GET',
+    url: '/visualization.json',
+    async: false,
+    dataType: 'json',
+    success: function(data){
+      drawPieChart(data);
+      console.log('success');
+    },
+    failure: function(result){
+      alert('ERROR');
+    }
+  })
+}
+function drawPieChart(data){
+  var discriminatoryData = data.filter(function (datum) {
+    return datum.discriminatory;
+  });
+  var groupedData = [
+    { label: "Discriminatory", size: discriminatoryData.length },
+    { label: "Non-Discriminatory", size: data.length - discriminatoryData.length }
+  ];
+  d3plus.viz()
+    .container("#viz")
+    .data(groupedData)
+    .type("pie")
+    .id("label")
+    .size("size")
+    .draw();
+}
+$(document).ready(function(){
+  loadData();
+})

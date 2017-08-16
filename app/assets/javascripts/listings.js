eb123b0be98801d3e59138ad95fedcc6dc65cc6c
Steve Hackbarth <stephenhackbarth@gmail.com>
2017-08-15 20:58:13 -0400

    enlarge chart and add percentages

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -17,9 +17,19 @@ function drawPieChart(data){
   var discriminatoryData = data.filter(function (datum) {
     return datum.discriminatory;
   });
+  var discriminatoryCount = discriminatoryData.length;
+  var nonDiscriminatoryCount = data.length - discriminatoryCount;
+  var discriminatoryPercent = (100 * discriminatoryCount / data.length).toFixed(0) + "%";
+  var nonDiscriminatoryPercent = (100 * nonDiscriminatoryCount / data.length).toFixed(0) + "%";
   var groupedData = [
-    { label: "Discriminatory", size: discriminatoryData.length },
-    { label: "Non-Discriminatory", size: data.length - discriminatoryData.length }
+    {
+      label: "Discriminatory (" + discriminatoryPercent + ")",
+      size: discriminatoryData.length
+    },
+    {
+      label: "Non-Discriminatory (" + nonDiscriminatoryPercent + ")",
+      size: data.length - discriminatoryData.length
+    }
   ];
   d3plus.viz()
     .container("#viz")
